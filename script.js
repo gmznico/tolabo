433218481b0098dc97819315ebcfbdef72bd84aa
gmznico <93000085+gmznico@users.noreply.github.com>
2021-10-22 12:58:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+jQuery('document').ready(function($){
+    var menuBtn = $('.menu-icon'), menu = $('.nav ul');
+
+    menuBtn.click(function(){
+        
+        if(menu.hasClass('show')){
+            menu.removeClass('show');
+        }else{
+            menu.addClass('show');
+        }
+    });
+
+});
+
+$(document).ready(function(){
+
+	// AGREGANDO CLASE ACTIVE AL PRIMER ENLACE ====================
+	$('.category-lists .category-item[category="all"]').addClass('ct_item-active');
+
+	// FILTRANDO PRODUCTOS  ============================================
+
+	$('.category-item').click(function(){
+		var catProduct = $(this).attr('category');
+		console.log(catProduct);
+
+		// AGREGANDO CLASE ACTIVE AL ENLACE SELECCIONADO
+		$('.category-item').removeClass('ct_item-active');
+		$(this).addClass('ct_item-active');
+
+		// OCULTANDO PRODUCTOS =========================
+		$('.product-item').css('transform', 'scale(0)');
+		function hideProduct(){
+			$('.product-item').hide();
+		} setTimeout(hideProduct,400);
+
+		// MOSTRANDO PRODUCTOS =========================
+		function showProduct(){
+			$('.product-item[category="'+catProduct+'"]').show();
+			$('.product-item[category="'+catProduct+'"]').css('transform', 'scale(1)');
+		} setTimeout(showProduct,400);
+	});
+
+	// MOSTRANDO TODOS LOS PRODUCTOS =======================
+
+	$('.category-item[category="all"]').click(function(){
+		function showAll(){
+			$('.product-item').show();
+			$('.product-item').css('transform', 'scale(1)');
+		} setTimeout(showAll,400);
+	});
+});
